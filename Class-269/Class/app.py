9f942cfa3934a265dff4d686befe9b8baa281915
Bunny Pranav <pranavvasipalli@gmail.com>
2024-05-19 12:15:25 +0530

    Class 270 .env addition

--- Class-269/Class/app.py
+++ Class-269/Class/app.py
@@ -1,7 +1,12 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from faker import Faker
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import SyncGrant
+
+load_dotenv(dotenv_path=Path("/Class-269/Class/.env"))
 
 app = Flask(__name__)
 
@@ -15,6 +20,11 @@ def index():
 def generate_token():
     fake = Faker()
     username = request.args.get("username", fake.user_name())
+
+    account_sid = os.environ.get("account_sid")
+    api_key = os.environ.get("api_key")
+    api_secret = os.environ.get("api_secret")
+    service_SID = os.environ.get("service_SID")
 
     token = AccessToken(account_sid, api_key, api_secret, identity=username)
     sync_grant_access = SyncGrant(service_sid=service_SID)
